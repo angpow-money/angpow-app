928607873e039032a169a08978fcebbfc0d61b48
yikkai95 <yikkai95@gmail.com>
2024-09-21 22:46:55 +0800

    feat: fetch balance using fetch call

--- src/pages/api/balance.json.ts
+++ src/pages/api/balance.json.ts
@@ -1,17 +1,15 @@
-import { getBalance } from '@wagmi/core';
 import type { APIRoute } from 'astro';
-import { $config } from '@/stores/wallet';
+import { getBalance } from './_util';
 
 export const GET: APIRoute = async ({ request }) => {
   
   const address = new URL(request.url).searchParams.get('address')
 
-  const config = $config.get() as any;
-  const balance = await getBalance(config, { address: address as any })
+  const balance = await getBalance(address)
+  console.log(balance)
   //if (error) return new Response(JSON.stringify(error));
 
   return Response.json({
-    amount: balance.formatted
+    amount: (Number(balance) / 1e18).toString()
   })
 }
-
