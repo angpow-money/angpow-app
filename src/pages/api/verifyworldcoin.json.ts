b434e5e0b308046c8c2ef113ac140cbbdddceb94
yongfeng <yongfeng99@gmail.com>
2024-09-22 01:42:54 +0800

    try cloud verification

--- src/pages/api/verifyworldcoin.json.ts
+++ src/pages/api/verifyworldcoin.json.ts
@@ -14,25 +14,35 @@ export const POST: APIRoute = async ({ request }) => {
 
 const verifyProof = async (proof:any, action:any) => {
     console.log('proof', proof);
-    const response = await fetch(
-        // 'https://developer.worldcoin.org/api/v1/verify/app_staging_1fe6ccaa14409704f71091493087e46f',
-        'https://developer.worldcoin.org/api/v1/verify/app_3fce0a48811b44d2fc3e452699a480d4',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ ...proof, action: action}),
+    
+    try {
+
+        const response = await fetch(
+            // 'https://developer.worldcoin.org/api/v1/verify/app_staging_1fe6ccaa14409704f71091493087e46f',
+            'https://developer.worldcoin.org/api/v1/verify/app_3fce0a48811b44d2fc3e452699a480d4',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ ...proof, action: action}),
+            }
+        );
+        console.log('aavvvvvvv', response)
+        
+        if (response.ok) {
+            const dat = await response.json();
+            // console.log('dat', dat);
+            return dat;
+        } else {
+            // const { code, detail } = await response.json();
+            // throw new Error(`Error Code ${code}: ${detail}`);
+            // throw new Error(`failed.`);
+            return { "success": false };
         }
-    );
-    if (response.ok) {
-        const dat = await response.json();
-        // console.log('dat', dat);
-        return dat;
-    } else {
-        // const { code, detail } = await response.json();
-        // throw new Error(`Error Code ${code}: ${detail}`);
-        // throw new Error(`failed.`);
-        return { "success": false };
+
+    } catch(error) {
+        console.log('abbbb', error)
     }
+    
 };
